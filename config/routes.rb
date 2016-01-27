4fb82b67358e63dd9964a17f8fce70e216a00de1
Robert Cunningham <rgc3dev@gmail.com>
2016-01-27 18:01:40 -0500

    config routes, work on about, index and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  
+
+  get '/about' => 'welcome#about'
+
+  get  '/contact' => 'welcome#contact'
+
   root 'welcome#index'
-
-  get 'welcome/about'
-
-  get 'welcome/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
